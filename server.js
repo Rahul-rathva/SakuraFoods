695ef76ad6657cb36ed5dcd95e01937325247947
Rahul <160149113+Rahul-rathva@users.noreply.github.com>
2026-01-24 15:33:38 +0530

    server and db & modified metadata

---
+++ server.js
@@ -0,0 +1,15 @@
+const express= require('express');
+const mongoose = require ('mongoose');
+const dotenv= require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+const app=express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("Blossomfoods database connected!!"))
+.catch(err=> console.log(err));
+
+app.listen(3000, ()=> console.log("sever is running on port 3000"));
